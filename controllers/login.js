dfe5eb48ff4d7acc39996393dcd147e1828865fe
PatrickFS18 <Patrick.souza@aluno.riogrande.ifrs.edu.br>
2023-08-27 16:41:28 -0300

    login funcionando

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,29 @@
 const { sequelize, Sequelize } = require("../config/connection"); // Certifique-se de importar Sequelize
 const usuario = require( "../models/usuario" )(sequelize, Sequelize); // Passando os parâmetros necessários
 
-// Resto do código do controller...
+const bcrypt = require("bcrypt");
+
 
 exports.home = (req, res) => {
 
     res.render("login", { layout: false });
 };
-exports.login = (req, res) => {
 
-    res.render("login", { layout: false });
-};
+exports.login = async (req, res) => {
+    const { email, senha } = req.body;
+  
+    try {
+      // Busque o usuário pelo email no banco de dados
+      const user = await usuario.findOne({ where: { email } });
+  
+      if (!user || !(await bcrypt.compare(senha, user.senha))) {
+        return res.render("login", { errorMessage: "Credenciais inválidas." });
+      }
+  
+      // Se as credenciais estiverem corretas, redirecione para a página de dashboard
+      res.redirect("/");
+    } catch (error) {
+      console.error("Erro ao processar o login:", error);
+      res.render("login", { errorMessage: "Erro ao processar o login." });
+    }
+  };
