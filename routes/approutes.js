1c6951d069c30006c53f64fd5167648e4a35c410
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-08-28 11:09:06 -0300

    Add files via upload

--- routes/approutes.js
+++ routes/approutes.js
@@ -5,13 +5,22 @@ const router = express.Router();
 const myController = require("../controllers/mycontroller");
 const login = require("../controllers/login");
 const register = require("../controllers/register");
+const admin = require("../controllers/admin");
 
-router.get("/",myController.home);
-router.get("/register",register.home);
-router.get("/login",login.home);
+router.get("/", login.home);
+router.get("/register", register.home);
+router.get("/home",myController.home);
+
+router.get("/adicionar_livro", admin.adicionar_home);
+router.get("/administrar", admin.administrar_home);
 
 router.post("/login",login.login);
 router.post("/register",register.register);
 
+router.post("/adicionar_livro", admin.adicionar_livro);
+
+
+router.delete('/excluir_usuario/:id', admin.excluirUsuario);
+router.delete('/excluir_livro/:id', admin.excluirLivro);
 
 module.exports = router;
