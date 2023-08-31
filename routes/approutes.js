432a4d9fb282247d1e7a1062d9ac2cc0b767cf60
l <61829586+Whesley696@users.noreply.github.com>
2023-08-30 21:28:11 -0300

    search

--- routes/approutes.js
+++ routes/approutes.js
@@ -7,15 +7,20 @@ const login = require("../controllers/login");
 const register = require("../controllers/register");
 const admin = require("../controllers/admin");
 
+router.get("/search", myController.search);
+
 router.get("/", login.home); // "/" é login
 router.get("/register", register.home); // "/register" é registro
-router.get("/login",myController.exibirLivros); // "/login" é logado
+router.get("/login", myController.exibirLivros); // "/login" é logado
 
 router.get("/adicionar_livro", admin.adicionar_home);
 router.get("/administrar", admin.administrar_home);
 
-router.post("/login",login.login);
-router.post("/register",register.register);
+router.post("/login", login.login);
+router.post("/register", register.register);
+
+router.post("/ordenar", myController.buscarPorAno);
+router.post("/buscar", myController.buscarPorTitulo);
 
 router.post("/adicionar_livro", admin.adicionar_livro);
 router.post('/alugar_livro/:id', myController.alugarLivro);
@@ -23,5 +28,7 @@ router.post('/alugar_livro/:id', myController.alugarLivro);
 
 router.delete('/excluir_usuario/:id', admin.excluirUsuario);
 router.delete('/excluir_livro/:id', admin.excluirLivro);
+
+
 
 module.exports = router;
